5ef0e80d05a2aa6d6b4e0edc6ed971e730b1cc9e
Italo Qualisoni <italo.qualisoni@acad.pucrs.br>
2015-02-28 01:13:55 -0300

    Renamed the directory names

---
+++ Algorithms/Warmup/Solve Me First/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    static int solveMeFirst(int a, int b) {
+        return a+b;
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(solveMeFirst(in.nextInt(), in.nextInt()));
+   }
+}
